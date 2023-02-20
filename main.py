77354e21f0b56991f6f62024ab13ff4a9be9fef0
web3-king <jacobroberto950212@gmail.com>
2023-02-20 14:20:11 +0800

    load bs4

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)    
+soup = BeautifulSoup(page.content, "html.parser")
+
+# print(page.text)    
