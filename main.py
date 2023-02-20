b6b47e9a6b98442965ca42560098238e22344be8
web3-king <jacobroberto950212@gmail.com>
2023-02-20 15:06:17 +0800

    find python jobs

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="ResultsContainer")
 
-print(results.prettify())    
+job_elements = results.find_all("div", class_="card-content")
+
+# for job_element in job_elements:
+#     title_element = job_element.find("h2", class_="title")
+#     company_element = job_element.find("h3", class_="company")
+#     location_element = job_element.find("p", class_="location")
+#     print(title_element.text)
+#     print(company_element.text)
+#     print(location_element.text)
+#     print()
+
+python_jobs = results.find_all("h2", string=lambda text : "python" in text.lower())
+
+print(python_jobs)
+# print(results.prettify())    
